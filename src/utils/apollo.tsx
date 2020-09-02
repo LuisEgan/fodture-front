9d266e4979af2bdb70642c58fa20d87fd6952ed4
L <l@cleveritgroup.com>
2020-09-01 21:25:13 -0400

    hardcoded fuck

--- src/utils/apollo.tsx
+++ src/utils/apollo.tsx
@@ -10,7 +10,9 @@ let apolloClient: ApolloClient<NormalizedCacheObject> | undefined;
 
 function createIsomorphLink() {
   return new HttpLink({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_API_ENDPOINT, // Server URL (must be absolute)
+    // uri: process.env.NEXT_PUBLIC_GRAPHQL_API_ENDPOINT, // Server URL (must be absolute)
+    uri: "https://fodture.azurewebsites.net/graphql",
+
     credentials: "same-origin", // Additional fetch() options like `credentials` or `headers`
   });
 }
